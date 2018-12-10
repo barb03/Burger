1d932f02bd87cdb4f3ab39186e0f8f94a5a2954d
Vader55 <38086201+barb03@users.noreply.github.com>
2018-12-10 12:33:24 -0600

    added heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,35 @@
 var mysql = require("mysql");
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,  
-    password: process.env.DB_PASS,
-    database: "burger_db"
-  });
-  
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }  
-    console.log("connected as id " + connection.threadid);
-  });
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);  
+}
+else{
+  connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,  
+        password: process.env.DB_PASS,
+        database: "burger_db"
+      });
+};
 
+// var connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     port: 3306,
+//     user: process.env.DB_USER,  
+//     password: process.env.DB_PASS,
+//     database: "burger_db"
+//   });
+  
+//   connection.connect(function(err) {
+//     if (err) {
+//       console.error("error connecting: " + err.stack);
+//       return;
+//     }  
+//     console.log("connected as id " + connection.threadid);
+//   });
+
+  connection.connect();
   module.exports = connection;
